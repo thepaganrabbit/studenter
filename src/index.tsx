8c38cb22ca3f587390a92a2054513200af6c97c5
thepaganrabbit <gonzalesgustavoc@gmail.com>
2022-06-15 10:48:47 -0600

    enhancement: added Bulma to handle base line styling.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'bulma/css/bulma.css';
+
+import App from './App';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
